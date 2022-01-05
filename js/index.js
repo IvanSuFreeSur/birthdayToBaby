c7196f9fe2278db94bfb367418bc7806cd2ef629
IvanSu <>
2022-01-05 23:38:04 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		
+    if(userName=="蔡佳芸" &&  pwd=="0121"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("你不是我的佳芸寶貝!! 別偷看");
+	}
+});
